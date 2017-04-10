f904da348ac6358e4a2dca97d2a817b609563718
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-04-10 22:59:32 +0300

    add CrimeFragment to CrimeActivity

--- app/src/main/java/com/leo/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.leo.android.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,5 +12,15 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+            .add(R.id.fragmentContainer, fragment)
+            .commit();
+        }
     }
 }
