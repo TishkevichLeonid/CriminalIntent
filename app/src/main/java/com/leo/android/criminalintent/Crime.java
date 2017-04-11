c272079c4fd44050407688efba2b6959d08eb3da
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-04-11 13:24:55 +0300

    add widgets to fragmet crime

--- app/src/main/java/com/leo/android/criminalintent/Crime.java
+++ app/src/main/java/com/leo/android/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.leo.android.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -10,9 +11,28 @@ public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         mId = UUID.randomUUID();
+        mDate = new Date();
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 
     public UUID getId() {
