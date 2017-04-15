b940840a8bde6acc0dc0fac38f991cd146a697cf
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-04-15 16:25:09 +0300

    star to make controllers

--- app/src/main/java/com/leo/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeActivity.java
@@ -5,21 +5,10 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
-
-        if (fragment == null){
-            fragment = new CrimeFragment();
-            fm.beginTransaction()
-            .add(R.id.fragmentContainer, fragment)
-            .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
