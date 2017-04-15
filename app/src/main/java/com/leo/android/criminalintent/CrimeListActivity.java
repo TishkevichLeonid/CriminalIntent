7ec583c8ffd5a5c4166716d272e32ee700ed5544
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-04-15 19:49:34 +0300

    1

---
+++ app/src/main/java/com/leo/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.leo.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by leonidtiskevic on 15.04.17.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
---
+++ app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.leo.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by leonidtiskevic on 15.04.17.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
