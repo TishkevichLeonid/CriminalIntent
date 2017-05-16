1ebc871c317bcc831a6d08b0a5742c86df4a8366
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-05-16 16:37:55 +0300

    add crimelistfragment

--- app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
@@ -1,10 +1,29 @@
 package com.leo.android.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by leonidtiskevic on 15.04.17.
  */
 
 public class CrimeListFragment extends Fragment {
+
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = (RecyclerView) view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
