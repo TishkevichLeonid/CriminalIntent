67d52a8c0444f6e4d3404cee7cb2ae9818558c00
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-05-22 23:11:12 +0300

    make CrimePagerActivity for swipe

---
+++ app/src/main/java/com/leo/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,46 @@
+package com.leo.android.criminalintent;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+
+/**
+ * Created by leonidtiskevic on 22.05.17.
+ */
+
+public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+    }
+
+}
