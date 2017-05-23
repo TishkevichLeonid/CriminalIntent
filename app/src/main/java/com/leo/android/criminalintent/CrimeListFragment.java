d0c6b6270de9707a535aff81c394d2438ece5cd3
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-05-23 14:30:07 +0300

    add Pager and swype

--- app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
@@ -77,7 +77,7 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            Intent intent = CrimePagerActivity.newInstance(getActivity(), mCrime.getId());
             startActivity(intent);
 
         }
--- app/src/main/java/com/leo/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimePagerActivity.java
@@ -1,5 +1,7 @@
 package com.leo.android.criminalintent;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
@@ -10,19 +12,30 @@ import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by leonidtiskevic on 22.05.17.
  */
 
 public class CrimePagerActivity extends FragmentActivity {
+    public static final String EXTRA_CRINE_ID = "com.leo.android.criminalintent.crime_id";
+
     private ViewPager mViewPager;
     private List<Crime> mCrimes;
+
+    public static Intent newInstance(Context packageContext, UUID crimeId){
+        Intent intent = new Intent(packageContext, CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRINE_ID, crimeId);
+        return intent;
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRINE_ID);
 
         mViewPager = (ViewPager) findViewById(R.id.activity_crime_pager_view_pager);
 
