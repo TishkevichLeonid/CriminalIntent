6cd0a5f8a7c5e96df8b3edcf8b987feabeeb7b4c
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-06-09 12:06:07 +0300

    start to make TimePicker

---
+++ app/src/main/java/com/leo/android/criminalintent/TimePickerFragment.java
@@ -0,0 +1,10 @@
+package com.leo.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by leonidtiskevic on 09.06.17.
+ */
+
+public class TimePickerFragment extends DialogFragment {
+}
