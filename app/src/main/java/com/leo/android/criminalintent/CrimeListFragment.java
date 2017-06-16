004709dde868448206b8bd18bc08a06e03f93819
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-06-16 17:12:11 +0300

    add subtittle

--- app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeListFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -78,10 +79,23 @@ public class CrimeListFragment extends Fragment {
                         newIntent(getActivity(), crime.getId());
                 startActivity(intent);
                 return true;
+            case R.id.menu_item_show_subtitle:
+                updateSubtitle();
+                return true;
+
             default:
                     return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    public void updateSubtitle(){
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        int crimeCount = crimeLab.getCrimes().size();
+        String subtitle = getString(R.string.subtitle_format, crimeCount);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.getSupportActionBar().setSubtitle(subtitle);
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
