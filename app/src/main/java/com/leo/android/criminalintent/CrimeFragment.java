34555ddebec898f5c9bc6f6ecb36e5214758ad6d
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-06-23 18:40:49 +0300

    choose ShareCompat.IntentBuilder.form

--- app/src/main/java/com/leo/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ShareCompat;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
@@ -139,12 +140,12 @@ public class CrimeFragment extends Fragment {
         mReportButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("text/plain");
-                i.putExtra(Intent.EXTRA_TEXT, getReport());
-                i.putExtra(Intent.EXTRA_SUBJECT, R.string.crime_report_subject);
-                i = Intent.createChooser(i, getString(R.string.send_report));
-                startActivity(i);
+                ShareCompat.IntentBuilder.from(getActivity())
+                        .setType("text/[lain")
+                        .setText(getReport())
+                        .setSubject(getString(R.string.crime_report_subject))
+                        .setChooserTitle(getString(R.string.send_report))
+                        .startChooser();
             }
         });
 
