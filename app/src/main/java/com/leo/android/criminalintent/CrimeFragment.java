4b2560dfe067742e9f8200cc721092a6a7bd8d57
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-07-05 12:42:59 +0300

    add photoView

--- app/src/main/java/com/leo/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/leo/android/criminalintent/CrimeFragment.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -198,6 +199,7 @@ public class CrimeFragment extends Fragment {
         });
 
         mPhotoView = (ImageView) v.findViewById(R.id.crime_photo);
+        updatePhotoView();
 
         return v;
     }
@@ -260,6 +262,8 @@ public class CrimeFragment extends Fragment {
             } finally {
                 c.close();
             }
+        } else if (requestCode == REQUEST_PHOTO){
+            updatePhotoView();
         }
 
     }
@@ -301,5 +305,14 @@ public class CrimeFragment extends Fragment {
                 dateString, solvedString, suspect);
 
         return report;
+    }
+
+    private void updatePhotoView(){
+        if (mPhotoFile == null || !mPhotoFile.exists()){
+            mPhotoView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = PicturesUtils.getScaledBitmap(mPhotoFile.getPath(), getActivity());
+            mPhotoView.setImageBitmap(bitmap);
+        }
     }
 }
---
+++ app/src/main/java/com/leo/android/criminalintent/PicturesUtils.java
@@ -0,0 +1,47 @@
+package com.leo.android.criminalintent;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+
+/**
+ * Created by leonidtiskevic on 02.07.17.
+ */
+
+public class PicturesUtils {
+
+    public static Bitmap getScaledBitmap(String path, Activity activity){
+        Point size = new Point();
+
+        activity.getWindowManager().getDefaultDisplay().getSize(size);
+
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight){
+     // Чтение размеров изображения на диске
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        // Вычисление степени маштабирования
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            if (srcWidth > srcHeight) {
+                inSampleSize = Math.round(srcHeight / destHeight);
+            } else {
+                inSampleSize = Math.round(srcWidth / destWidth);
+            }
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        // Чтение данных и создание итогового изображения
+        return BitmapFactory.decodeFile(path, options);
+    }
+}
